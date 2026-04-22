97d21a7a64af9db3fb158daf9c0ea4171de32651
Shams Alghof <shamsalghof@gmail.com>
2026-04-22 22:33:48 +0300

    Conflict fix

--- python_stack/_python/OOP/UserswithBankAccounts/Users _with _Bank _Accounts.py
+++ python_stack/_python/OOP/UserswithBankAccounts/Users _with _Bank _Accounts.py
@@ -1,5 +1,29 @@
 # Import BankAccount class from another folder
-from BankAccount.BankAccount import BankAccount 
+class BankAccount:
+    def __init__(self,int_rate=0.01,balance=0):
+        self.int_rate=int_rate
+        self.balance=balance
+    
+    def deposit(self, amount):
+        self.balance+=amount
+        return self
+
+    def withdraw(self, amount):
+        if self.balance>=amount:
+            self.balance-=amount
+        else:
+            print("Insufficient funds: Charging a $5 fee")
+            self.balance-=5
+        return self
+
+    def display_account_info(self):
+        print(f"balance:$ {self.balance}")
+        return self
+
+    def yield_interest(self):
+        if self.balance>0:
+            self.balance=self.balance+(self.balance*self.int_rate)
+        return self   
 
 
 class User:
